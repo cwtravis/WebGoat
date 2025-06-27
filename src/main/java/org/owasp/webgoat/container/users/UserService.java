234e8892d9b45ebdd3eefb50f2b0d35ab4d94b33
Dev 1 <cwtravis@gmail.com>
2025-06-27 08:00:27 -0400

    Add SQLi Example
    Adding SQLi

--- src/main/java/org/owasp/webgoat/container/users/UserService.java
+++ src/main/java/org/owasp/webgoat/container/users/UserService.java
@@ -52,6 +52,9 @@ public class UserService implements UserDetailsService {
   private void createLessonsForUser(WebGoatUser webGoatUser) {
     jdbcTemplate.execute("CREATE SCHEMA \"" + webGoatUser.getUsername() + "\" authorization dba");
     flywayLessons.apply(webGoatUser.getUsername()).migrate();
+
+    //Add another SQLi
+      jdbcTemplate.execute("CREATE SCHEMA \"" + webGoatUser.getUsername() + "\" authorization dba");
   }
 
   public List<WebGoatUser> getAllUsers() {
